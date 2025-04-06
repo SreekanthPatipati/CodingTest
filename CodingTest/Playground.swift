2fcaad11412bea745f59895c0db75dcffa4e6799
SreekanthPatipati <psreekanth243@gmail.com>
2025-04-06 09:14:50 +0530

    rework coding...

--- CodingTest/Playground.swift
+++ CodingTest/Playground.swift
@@ -8,14 +8,14 @@
 import UIKit
 
 class PlayGround: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         
         // MARK: - remove the nil values
-        var userArray = ["1", "Hello", "3", "nil", "5"]
+        let userArray = ["1", "Hello", "3", "nil", "5"]
         var stringArray:[String] = []
         
         for valu in userArray { //["1", "Hello", "3", "nil", "5"]
@@ -24,17 +24,17 @@ class PlayGround: UIViewController {
             }
         }
         print(stringArray) //["1", "Hello", "3", "5"]
-
+        
         
         // MARK: - filter String words only
-        var userArrayValue = ["1","2","3","Sree","Kanth"]
+        let userArrayValue = ["1","2","3","Sree","Kanth"]
         for valu in userArrayValue { //["1","2","3","Sree","Kanth"]
             if Int(valu) == nil {
                 stringArray.append(valu)
             }
         }
         print(stringArray) //["Sree", "Kanth"]
-
+        
         // MARK: - filter Int Values words only
         for valu in userArrayValue { //["1","2","3","Sree","Kanth"]
             if Int(valu) != nil {
@@ -42,18 +42,18 @@ class PlayGround: UIViewController {
             }
         }
         print(stringArray) //["1", "2", "3"]
-
+        
         // MARK: - remove nil value
-        var userArrayNil = ["1", "Hello", "3", nil, "5"]
+        let userArrayNil = ["1", "Hello", "3", nil, "5"]
         for valu in userArrayNil { //["1", "Hello", "3", nil, "5"]
             if let strs = valu {
                 stringArray.append(strs)
             }
         }
         print(stringArray) //["1", "Hello", "3", "5"]
-
+        
         // MARK: - remove nil values
-        var userNumbers = [1, nil, 3, nil, 5, 6, nil, 8]
+        let userNumbers = [1, nil, 3, nil, 5, 6, nil, 8]
         var integerArray:[Int] = []
         
         for valu in userNumbers { //[1, nil, 3, nil, 5, 6, nil, 8]
@@ -64,23 +64,23 @@ class PlayGround: UIViewController {
         print(integerArray) //[1, 3, 5, 6, 8]
         
         // MARK: - reverse palindrom
-        var reversArray = ["h", "e", "l", "l", "o"]
+        let reversArray = ["h", "e", "l", "l", "o"]
         for valu in reversArray { //["h", "e", "l", "l", "o"]
             stringArray.insert(valu, at: 0)
         }
         print(stringArray) //["o", "l", "l", "e", "h"]
         
         // MARK: - count the array
-        var countArray = ["apple", "banana", "apple", "orange", "banana", "banana"]
+        let countArray = ["apple", "banana", "apple", "orange", "banana", "banana"]
         var stringCount:[String: Int] = [:]
-      
+        
         for valu in countArray { // ["apple", "banana", "apple", "orange", "banana", "banana"]
             stringCount[valu, default: 0] += 1
         }
         print(stringCount) // ["apple": 2, "orange": 1, "banana": 3]
-
+        
         // MARK: - Longest word and Shortest word
-        var longAndShortArray = ["Swift", "Programming", "HigherOrderFunctions", "Sree"]
+        let longAndShortArray = ["Swift", "Programming", "HigherOrderFunctions", "Sree"]
         var stringLenth = longAndShortArray[0]
         
         for valu in longAndShortArray { // ["Swift", "Programming", "HigherOrderFunctions", "Sree"]
@@ -89,7 +89,7 @@ class PlayGround: UIViewController {
             }
         }
         print(stringLenth) // HigherOrderFunctions
-
+        
         
         // MARK: - Ascending order and desending order
         var sortingArray = [9,8,1,2,4,5,6,7]
@@ -103,25 +103,25 @@ class PlayGround: UIViewController {
             }
         }
         print(sortingArray) // [1, 2, 4, 5, 6, 7, 8, 9]
-
+        
         
         // MARK: - Higest Number and Lowest Number
-        var higherNumberArray = [9,8,1,2,4,5,6,7]
+        let higherNumberArray = [9,8,1,2,4,5,6,7]
         var highestNumber = higherNumberArray[0]
-
+        
         for valu in higherNumberArray { // [9,8,1,2,4,5,6,7]
             if valu > highestNumber { // < lower Number.
                 highestNumber = valu
             }
         }
         print(highestNumber) // 9
-
-
+        
+        
         // MARK: - Second Highest and Second Lowest number
-        var secondHightAndLowestNumberArray = [9,8,1,2,4,5,6,7]
+        let secondHightAndLowestNumberArray = [9,8,1,2,4,5,6,7]
         var minimumNum1 = Int.min
         var minimumNum2 = Int.min
-
+        
         for valu in secondHightAndLowestNumberArray { // [9,8,1,2,4,5,6,7]
             if valu > minimumNum1 {
                 minimumNum2 = minimumNum1
@@ -131,10 +131,10 @@ class PlayGround: UIViewController {
             }
         }
         print(minimumNum2) // 8 or 2
-
+        
         
         // MARK: - remove duplicate values
-        var duplicateArray = [1, 2, 3, 4, 5, 1, 2]
+        let duplicateArray = [1, 2, 3, 4, 5, 1, 2]
         var filterArray1:[Int] = []
         var filterArray2:[Int] = []
         var filterArray3:[Int] = []
@@ -146,48 +146,48 @@ class PlayGround: UIViewController {
                 filterArray2.append(valu)
             }
         }
-
+        
         print(filterArray1) // [1, 2, 3, 4, 5]
         print(filterArray2) // [1, 2]
-
+        
         for valu in filterArray1 {
             if !filterArray2.contains(valu) {
                 filterArray3.append(valu)
             }
         }
         print(filterArray3) // [3, 4, 5]
-
+        
         
         // MARK: - addition numbers
-        var sumArray = [1, 3, 10, 2, 5]
+        let sumArray = [1, 3, 10, 2, 5]
         var sum = 0
-
+        
         for valu in sumArray { // [1, 3, 10, 2, 5]
             sum += valu
         }
         print(sum) // 21
-
+        
         
         // MARK: - multiply the numbers
         var multiplyArray = [1, 3, 10, 2, 5]
-
+        
         for valu in 0..<multiplyArray.count { // [1, 3, 10, 2, 5]
             multiplyArray[valu] *= 2
         }
         print(multiplyArray) // [2, 6, 20, 4, 10]
-
+        
         
         // MARK: - Square the number
         var multiplyArray2 = [1, 3, 10, 2, 5]
-
+        
         for valu in 0..<multiplyArray2.count { // [1, 3, 10, 2, 5]
             multiplyArray2[valu] = multiplyArray2[valu] * multiplyArray2[valu]
         }
         print(multiplyArray2) // [1, 9, 100, 4, 25]
-
+        
         
         // MARK: - Even numbers
-        var numbersArray = [1, 3, 10, 2, 5]
+        let numbersArray = [1, 3, 10, 2, 5]
         var evenNumber:[Int] = []
         for valu in numbersArray { // [1, 3, 10, 2, 5]
             if valu % 2 == 0 {
@@ -195,7 +195,7 @@ class PlayGround: UIViewController {
             }
         }
         print(evenNumber) // [10, 2]
-
+        
         
         // MARK: - Odd numbers
         var oddNumber:[Int] = []
@@ -205,52 +205,52 @@ class PlayGround: UIViewController {
             }
         }
         print(oddNumber) // [1, 3, 5]
-
-        var evenOddNumbers:[String:[Int]] = ["Even": evenNumber,"Odd": oddNumber]
+        
+        let evenOddNumbers:[String:[Int]] = ["Even": evenNumber,"Odd": oddNumber]
         print(evenOddNumbers) // ["Odd": [1, 3, 5], "Even": [10, 2]]
-      
-
+        
+        
         // MARK: - Swap the numbers
         var swapNumbers = [1,2,4,5,6,7,8,9]
         for i in stride(from: 0, to: swapNumbers.count - 1, by: 2) { // [1,2,4,5,6,7,8,9]
             swapNumbers.swapAt(i, i+1)
         }
         print(swapNumbers) // [2, 1, 5, 4, 7, 6, 9, 8]
-
-      
+        
+        
         // MARK: - Palindrom
-        var palindromString = "madam"
+        let palindromString = "madam"
         var palindromStr = ""
         
         for valu in palindromString { // "madam"
             palindromStr = String(valu) + palindromStr
         }
         print(palindromStr) // "madam"
-
+        
         // MARK: - Find the string value
-        var greeting = "Helloplayground"
-        var result = greeting.replacingOccurrences(of: "Hello", with: "")
+        let greeting = "Helloplayground"
+        let result = greeting.replacingOccurrences(of: "Hello", with: "")
         print(result) // playground
-
+        
         // MARK: - Singletone Example
         
         
         // MARK: - Enum Example
         
         
-
-
+        
+        
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
--- CodingTest/ViewController.swift
+++ CodingTest/ViewController.swift
@@ -8,101 +8,157 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         // MARK: - higher-order functions
         
+        // MARK: - remove the nil values
         var userArray = ["1", "Hello", "3", "nil", "5"]
+        let aa = userArray.filter { $0 != "nil" }
+        print(aa) // ["1", "Hello", "3", "5"]
+        
+        // MARK: - filter String words only
         var userArrayValue = ["1","2","3","Sree","Kanth"]
+        let zz = userArrayValue.filter { Int($0) == nil }
+        print(zz) // ["Sree", "Kanth"]
+        let qq = userArrayValue.filter { Int($0) != nil }
+        print(qq) // ["1", "2", "3"]
+        
+        // MARK: - remove nil value
         var userArrayNil = ["1", "Hello", "3", nil, "5"]
+        let pp = userArrayNil.compactMap { $0 }
+        print(pp) // ["1", "Hello", "3", "5"]
+        
+        // MARK: - remove nil values
         var userNumbers = [1, nil, 3, nil, 5, 6, nil, 8]
+        let xx = userNumbers.compactMap { $0 }
+        print(xx) // [1, 3, 5, 6, 8]
+        
+        // MARK: - reverse palindrom
         var reversArray = ["h", "e", "l", "l", "o"]
+        // MARK: - count the array
         var countArray = ["apple", "banana", "apple", "orange", "banana", "banana"]
+        
+        
+        // MARK: - Longest word and Shortest word
         var longAndShortArray = ["Swift", "Programming", "HigherOrderFunctions", "Sree"]
+        if let longest = longAndShortArray.max(by: { $0.count > $1.count }) {
+            print("Longest word: \(longest)")
+        } // "HigherOrderFunctions" or // Sree
+        
+        let uu = longAndShortArray.description.lowercased()
+        print(uu) //
+        
+        let uuuu = longAndShortArray.description.uppercased()
+        print(uuuu) //
+        
+        let uuu = longAndShortArray.description.capitalized
+        print(uuu) //
+        
+        // MARK: - Ascending order and desending order
         var sortingArray = [9,8,1,2,4,5,6,7]
+        let ss = sortingArray.sorted(by: >)
+        print(ss) // [9, 8, 7, 6, 5, 4, 2, 1]
+        let ww = sortingArray.sorted(by: <)
+        print(ww) // [1, 2, 4, 5, 6, 7, 8, 9]
+        
+        // MARK: - Higest Number and Lowest Number
         var higherNumberArray = [9,8,1,2,4,5,6,7]
+        let ee = higherNumberArray.max()!
+        print(ee) // 9
+        let dd = higherNumberArray.min()!
+        print(dd) // 1
+        
+        // MARK: - Second Highest and Second Lowest number
         var secondHightAndLowestNumberArray = [9,8,1,2,4,5,6,7]
+        let cc = secondHightAndLowestNumberArray.sorted(by: <).dropFirst().first!
+        print(cc) // 2
+        let rr = secondHightAndLowestNumberArray.sorted(by: >).dropFirst().first!
+        print(rr) // 8
+        
+        // MARK: - remove duplicate values
         var duplicateArray = [1, 2, 3, 4, 5, 1, 2]
+        let ll = Array(Set(duplicateArray))
+        print(ll) // [1, 4, 2, 3, 5]
+        
+        let countedSet = NSCountedSet(array: duplicateArray)
+        let mm = duplicateArray.filter { countedSet.count(for: $0) == 1 }
+        print(mm) // [3, 4, 5]
+        
+        let countedSet2 = NSCountedSet(array: duplicateArray)
+        let nn = Set(duplicateArray.filter { countedSet2.count(for: $0) > 1 })
+        print(nn) // [1,2]
+        
+        // MARK: - addition numbers
         var sumArray = [1, 3, 10, 2, 5]
+        let tt = sumArray.reduce(0, +)
+        print(tt) // 21
+        
+        // MARK: - multiply the numbers
         var multiplyArray = [1, 3, 10, 2, 5]
+        let gg = multiplyArray.map{ $0 * 2 }
+        print(gg)// [2, 6, 20, 4, 10]
+        
+        // MARK: - Square the number
         var multiplyArray2 = [1, 3, 10, 2, 5]
+        let bb = multiplyArray2.map{ $0 * $0 }
+        print(bb)// [1, 9, 100, 4, 25]
+        
+        // MARK: - Even numbers
         var numbersArray = [1, 3, 10, 2, 5]
+        let yy = numbersArray.filter { $0 % 2 == 0 }
+        print(yy) // [10, 2]
+        let hh = numbersArray.filter { $0 % 2 != 0 }
+        print(hh) // [1, 3, 5]
+        
+        // MARK: - Swap the numbers
         var swapNumbers = [1,2,4,5,6,7,8,9]
-        var palindromString = "madam"
-        var greeting = "Helloplayground"
+        for i in stride(from: 0, to: swapNumbers.count - 1, by: 2) {
+            swapNumbers.swapAt(i, i + 1)
+        }// [2, 1, 5, 4, 7, 6, 9, 8]
         
+        print("Swap first and last elements :- \(swapNumbers.swapAt(0, swapNumbers.count - 1))") // [9, 2, 4, 5, 6, 7, 8, 1]
         
-        
-        
-        /*
-        var greeting = "Hello, playground"
-
-        var a = [1,2,3,4,5]
-        var ar = [2,3,3,2,5,7,8,5]
-        var arr = ["1","2","3","Sree","Kanth"]
-        var arra = [1,2,3,nil,nil,4,5]
-        var array = ["Sree","Kanth","KANTH"]
-        var array44 = ["aa","ss","aa"]
-        var array55 = [[2,3],[3,4],[5,6]]
-
-        var sum = a.reduce(0, +)
-        var asc = a.sorted(by: >)
-        var desc = a.sorted(by: <)
-        var highestNumber = a.sorted(by: >).dropLast().first
-        var lowestnumber = a.sorted(by: <).dropLast().first
-        var hightsecond = a.sorted(by: >).dropFirst().first
-        var lowesecond = a.sorted(by: <).dropFirst().first
-
-        var nonduplicate = NSCountedSet(array: ar)
-        var gggg = ar.filter { nonduplicate.count(for: $0) == 1 }
-
-        var nonduplicate22 = NSCountedSet(array: array44)
-        var gggg333 = array44.filter { nonduplicate22.count(for: $0) == 1 }
-
-        var integersOnly = arr.compactMap { Int($0) }
-        var integerStrings = arr.filter { Int($0) == nil }
-        var lower = array.map { $0.lowercased() }
-        var upper = array.map { $0.uppercased() }
-        var loup = array.map { $0.capitalized }
-        var compmap = arra.compactMap { $0 }
-        var filtermap = array55.flatMap { $0 }
-        var st = arr.filter { Int($0) == nil }
-        var even = Dictionary(grouping: a) {
-            $0 % 2 == 0 ? "Even" : "Odd"
+        // Swap 2 and 5
+        if let index1 = swapNumbers.firstIndex(of: 2),
+           let index2 = swapNumbers.firstIndex(of: 5) {
+            swapNumbers.swapAt(index1, index2)
         }
-        var mapping = a.map { $0 * 2 }
-
+        print(swapNumbers) // [1, 5, 4, 2, 6, 7, 8, 9]
+        
+        
+        // MARK: - Palindrom
+        var palindromString = "madam"
+        let kk = String(palindromString.reversed())
+        print(kk) // madam
+        
+        // MARK: - Find the string value
+        var greeting = "Helloplayground"
+        let result = String(greeting.suffix("playground".count))
+        print(result) // playground
+        
+        // MARK: - FlatMap
+        var flatMaping = [[2, 3], [3, 4], [5, 6]]
+        let jj = flatMaping.flatMap { $0 }
+        print(jj) // [2, 3, 3, 4, 5, 6]
+        
         var arr1 = [1, 2, 3, 4, 5]
         var arr2 = [3, 4, 5, 6, 7]
         var commonElements = Array(Set(arr1).intersection(Set(arr2)))
         var commonElements33 = Array(Set(arr1).symmetricDifference(Set(arr2)))
-
-        var squared = a.map { $0 * $0 }
-        print(squared) // [1, 4, 9, 16, 25]
-
-        var values = ["1", "Hello", "3", nil, "5"]
-        var mappedValues = values.map { Int($0 ?? "") }
-        print(mappedValues) // [1, nil, 3, nil, 5]
-        var ssss = values.compactMap { Int($0 ?? "" )}
-
-        var validNumbers = values.compactMap { Int($0 ?? "") }
-        print(validNumbers) // [1, 3, 5]
-        var kkkk = a.filter { $0 % 2 == 0 }.map { $0 * $0 }
-        var numbersSet = [2, 3, 3, 2, 5, 7, 8, 5]
-        var numbersSet22 = Array(Set(numbersSet))
-
-
+        print(commonElements) // [3, 4, 5]
+        print(commonElements33) // [2, 7, 6, 1]
+        
         var namese = ["Alice", "Bob", "Charlie"]
         for (index, name) in namese.enumerated() {
             print("----> \(index): \(name)")
         }
-
+        
+        
         var numbers = [1, 2, 3, 4, 5]
-        // Get first even number
-        var fisteven = numbers.first { $0 % 2 == 0 }
         // Remove first 2 elements
         var withoutFirstTwo = numbers.dropFirst(2)
         print(withoutFirstTwo) // [3, 4, 5]
@@ -110,32 +166,20 @@ class ViewController: UIViewController {
         var withoutLastTwo = numbers.dropLast(2)
         print(withoutLastTwo) // [1, 2, 3]
         
-        var yy = "madam"
-        var yyyy = String(yy.reversed())
-
-        var arr11 = [1, 2, 3, 4, 5]
-        var arr22 = [3, 4, 5, 6, 7]
-
-        var pnr = Array(Set(arr11).intersection(Set(arr22)))
-        var pnr22 = Array(Set(arr11).symmetricDifference(Set(arr22)))
-
-        
-        
         var allSatisfy = [2, 4, 6, 8, 10]
         // Check if all numbers are even
         var allEven = allSatisfy.allSatisfy { $0 % 2 == 0 }
         print(allEven) // true
-
+        
         // Swift program to use sorted() function
         let wordss = ["apple", "banana", "cherry", "date"]
-          
         // Sorting an array of integers in descending order
         let sortedWords = wordss.sorted(by: { $0 > $1 })
         print(sortedWords)
         
-         */
+        
     }
-
-
+    
+    
 }
 
