245a1cd1ff18080bff1d16332c9263e926fabe42
SreekanthPatipati <psreekanth243@gmail.com>
2025-04-11 22:49:14 +0530

    interview Questions...

--- CodingTest/CodingRound2.swift
+++ CodingTest/CodingRound2.swift
@@ -14,6 +14,32 @@ class CodingRound2: UIViewController {
 
         // Do any additional setup after loading the view.
 
+        let userArray = ["1", "Hello", "3", "nil", "5"]
+        let userArrayValue = ["1","2","3","Sree","Kanth"]
+        let userArrayNil = ["1", "Hello", "3", nil, "5"]
+        let userNumbers = [1, nil, 3, nil, 5, 6, nil, 8]
+        var reversArray = ["h", "e", "l", "l", "o"]
+        var countArray = ["apple", "banana", "apple", "orange", "banana", "banana"]
+        let longAndShortArray = ["Swift", "Programming", "HigherOrderFunctions", "Sree"]
+        let sortingArray = [9,8,1,2,4,5,6,7]
+        let higherNumberArray = [9,8,1,2,4,5,6,7]
+        let secondHightAndLowestNumberArray = [9,8,1,2,4,5,6,7]
+        let duplicateArray = [1, 2, 3, 4, 5, 1, 2]
+        let sumArray = [1, 3, 10, 2, 5]
+        let multiplyArray = [1, 3, 10, 2, 5]
+        let multiplyArray2 = [1, 3, 10, 2, 5]
+        let numbersArray = [1, 3, 10, 2, 5]
+        var swapNumbers = [1,2,4,5,6,7,8,9]
+        let palindromString = "madam"
+        let greeting = "Helloplayground"
+        let flatMaping = [[2, 3], [3, 4], [5, 6]]
+        var arr1 = [1, 2, 3, 4, 5]
+        var arr2 = [3, 4, 5, 6, 7]
+        var namese = ["Alice", "Bob", "Charlie"]
+        var numbers = [1, 2, 3, 4, 5]
+        let wordss = ["apple", "banana", "cherry", "date"]
+        
+        
     }
     
 
